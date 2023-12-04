47079c65e21cb63e7ca17b25e226679aebdbe915
Mdsazib-sheikh <sazib.designer@gmail.com>
2023-12-04 22:16:48 +0600

    new file add

---
+++ arguments.js
@@ -0,0 +1,6 @@
+function getFullName(firstName, lastName){
+    const fullName = firstName + ' ' + lastName;
+    return fullName;
+}
+const identity = getFullName('omor', 'faruk', 'hanif', 'songkhet');
+console.log(identity);
