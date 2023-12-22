6c6f0c383f4a55d71bfc5731e1471eb0d2ebc310
Mdsazib-sheikh <sazib.designer@gmail.com>
2023-12-22 12:38:55 +0600

    new line added

--- defult.js
+++ defult.js
@@ -8,11 +8,19 @@
 // const total = add(14);
 // console.log(total);
 
-// another function use 
-function add(num1, num2){
-    num2 = num2 || 10;
-    return num1 + num2;
+                  // another function use 
+// function add(num1, num2){
+//     num2 = num2 || 10;
+//     return num1 + num2;
     
-}
-const total =add(15);
-console.log(total);
+// }
+// const total =add(15);
+// console.log(total);
+    //   more another functin use 
+    function add(num1, num2 = 10){
+        num2 = num2 || 0;
+        return num1 + num2;
+        
+    }
+    const total =add(15,1);
+    console.log(total);
